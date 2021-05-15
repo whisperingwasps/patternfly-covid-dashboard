72aadb13dc4f3d9cbe691dafb6c5da59ddb8944b
vishnupriyavr <papudear@gmail.com>
2021-05-15 14:06:06 +0000

    Introducing Twitter widgets

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
+import { Timeline,Tweet } from 'react-twitter-widgets'
 
 const Dashboard: React.FunctionComponent = () => (
   <PageSection>
     <Title headingLevel="h1" size="lg">Dashboard Page Title!</Title>
+    <Tweet tweetId="841418541026877441" options={{ width: "320" }} />
   </PageSection>
 )
 
