6edceecec3ac9e14b5005ae211ba48331cf56230
vishnupriyavr <papudear@gmail.com>
2021-05-15 14:29:58 +0000

    Twit Dark Mode + Grid

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
-import { PageSection, Title } from '@patternfly/react-core';
+import { PageSection, Title,Grid, GridItem } from '@patternfly/react-core';
 import { Timeline,Tweet } from 'react-twitter-widgets'
 
+const tweetArray = ["841418541026877441","841418541026877441","841418541026877441"]
 const Dashboard: React.FunctionComponent = () => (
   <PageSection>
-    <Title headingLevel="h1" size="lg">Dashboard Page Title!</Title>
-    <Tweet tweetId="841418541026877441" options={{ width: "320" }} />
+    <Title headingLevel="h1" size="lg">Information from Twitter</Title>
+    <Grid>
+      {tweetArray.map(
+      eachTweet => {
+                      return(
+                      <GridItem id={eachTweet} span={4}>
+                        <Tweet tweetId={eachTweet} options={{ width: "320",theme: "dark"  }}/>
+                      </GridItem>
+                      )
+                    }
+                    )}
+    </Grid>
+
   </PageSection>
 )
 
